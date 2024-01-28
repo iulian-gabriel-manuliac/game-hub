cd0623633ea59aaf0862b788138a3c6c9561634f
Iulian-Gabriel Manuliac <iulian.manuliac@globallogic.com>
2024-01-28 11:26:28 +0200

    build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -11,8 +12,8 @@ function App() {
           lg: `"nav nav" "aside main"`,
         }}
       >
-        <GridItem area="nav" bg="coral">
-          Nav
+        <GridItem area="nav">
+          <NavBar></NavBar>
         </GridItem>
         <Show above="lg">
           <GridItem area="aside" bg="gold">
