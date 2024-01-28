0321a472b4134395e5a32d30e182b2c7a0fbcc35
Iulian-Gabriel Manuliac <iulian.manuliac@globallogic.com>
2024-01-28 11:18:21 +0200

    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
