3409faeb7a184bccc83cba8781174707e249b08f
Iulian-Gabriel Manuliac <iulian.manuliac@globallogic.com>
2024-01-29 09:28:52 +0200

    games filter by platform

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,13 +4,15 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import { Genre } from "../hooks/useGenres";
+import { Platform } from "../hooks/usePlatforms";
 
 interface Props {
   selectedGenre: Genre | null;
+  selectedPlatform: Platform | null;
 }
 
-const GameGrid = ({ selectedGenre }: Props) => {
-  const { data, error, isLoading } = useGames(selectedGenre);
+const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
+  const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
@@ -28,11 +30,13 @@ const GameGrid = ({ selectedGenre }: Props) => {
             </GameCardContainer>
           ))}
         {!isLoading &&
+          data.length > 0 &&
           data.map((game) => (
             <GameCardContainer key={game.id}>
               <GameCard game={game} />
             </GameCardContainer>
           ))}
+        {!isLoading && data.length === 0 && <Text>No game found</Text>}
       </SimpleGrid>
     </>
   );
